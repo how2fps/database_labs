e63a0e601ce64db7692e1b46cacca58e511c842b
how2fps <hearthstonesigh@gmail.com>
2025-06-25 09:46:41 +0800

    ex4 done, no test yet

--- src/java/simpledb/storage/HeapFile.java
+++ src/java/simpledb/storage/HeapFile.java
@@ -1,14 +1,17 @@
 package simpledb.storage;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import simpledb.common.Database;
 import simpledb.common.DbException;
-import simpledb.common.Debug;
 import simpledb.common.Permissions;
 import simpledb.transaction.TransactionAbortedException;
 import simpledb.transaction.TransactionId;
-
-import java.io.*;
-import java.util.*;
 
 /**
  * HeapFile is an implementation of a DbFile that stores a collection of tuples
@@ -21,93 +24,171 @@ import java.util.*;
  * @author Sam Madden
  */
 public class HeapFile implements DbFile {
+       private final File f;
+       private final TupleDesc td;
 
-    /**
-     * Constructs a heap file backed by the specified file.
-     * 
-     * @param f
-     *            the file that stores the on-disk backing store for this heap
-     *            file.
-     */
-    public HeapFile(File f, TupleDesc td) {
-        // some code goes here
-    }
+       /**
+        * Constructs a heap file backed by the specified file.
+        * 
+        * @param f
+        *          the file that stores the on-disk backing store for this heap
+        *          file.
+        */
+       public HeapFile(File f, TupleDesc td) {
+              this.f = f;
+              this.td = td;
+       }
 
-    /**
-     * Returns the File backing this HeapFile on disk.
-     * 
-     * @return the File backing this HeapFile on disk.
-     */
-    public File getFile() {
-        // some code goes here
-        return null;
-    }
+       /**
+        * Returns the File backing this HeapFile on disk.
+        * 
+        * @return the File backing this HeapFile on disk.
+        */
+       public File getFile() {
+              return f;
+       }
 
-    /**
-     * Returns an ID uniquely identifying this HeapFile. Implementation note:
-     * you will need to generate this tableid somewhere to ensure that each
-     * HeapFile has a "unique id," and that you always return the same value for
-     * a particular HeapFile. We suggest hashing the absolute file name of the
-     * file underlying the heapfile, i.e. f.getAbsoluteFile().hashCode().
-     * 
-     * @return an ID uniquely identifying this HeapFile.
-     */
-    public int getId() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-    }
+       /**
+        * Returns an ID uniquely identifying this HeapFile. Implementation note:
+        * you will need to generate this tableid somewhere to ensure that each
+        * HeapFile has a "unique id," and that you always return the same value for
+        * a particular HeapFile. We suggest hashing the absolute file name of the
+        * file underlying the heapfile, i.e. f.getAbsoluteFile().hashCode().
+        * 
+        * @return an ID uniquely identifying this HeapFile.
+        */
+       public int getId() {
+              return f.getAbsoluteFile().hashCode();
+       }
 
-    /**
-     * Returns the TupleDesc of the table stored in this DbFile.
-     * 
-     * @return TupleDesc of this DbFile.
-     */
-    public TupleDesc getTupleDesc() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-    }
+       /**
+        * Returns the TupleDesc of the table stored in this DbFile.
+        * 
+        * @return TupleDesc of this DbFile.
+        */
+       public TupleDesc getTupleDesc() {
+              return td;
+       }
 
-    // see DbFile.java for javadocs
-    public Page readPage(PageId pid) {
-        // some code goes here
-        return null;
-    }
+       // see DbFile.java for javadocs
+       public Page readPage(PageId pid) {
+              int pageSize = BufferPool.getPageSize();
+              int pageNumber = pid.getPageNumber();
+              int offset = pageNumber * pageSize;
+              byte[] data = new byte[pageSize];
+              try (RandomAccessFile raf = new RandomAccessFile(f, "r")) {
+                     raf.seek(offset);
+                     raf.readFully(data);
+              } catch (IOException e) {
+                     throw new RuntimeException("Failed to read page from disk", e);
+              }
+              try {
+                     return new HeapPage((HeapPageId) pid, data);
+              } catch (IOException e) {
+                     throw new RuntimeException("Failed to create heap page from data", e);
+              }
+       }
 
-    // see DbFile.java for javadocs
-    public void writePage(Page page) throws IOException {
-        // some code goes here
-        // not necessary for lab1
-    }
+       // see DbFile.java for javadocs
+       public void writePage(Page page) throws IOException {
+              // some code goes here
+              // not necessary for lab1
+       }
 
-    /**
-     * Returns the number of pages in this HeapFile.
-     */
-    public int numPages() {
-        // some code goes here
-        return 0;
-    }
+       /**
+        * Returns the number of pages in this HeapFile.
+        */
+       public int numPages() {
+              long fileSize = f.length();
+              if (fileSize == 0) {
+                     return 0;
+              }
+              int pageSize = BufferPool.getPageSize();
+              int numberOfPages = (int) Math.floor((double) fileSize / pageSize);
+              return numberOfPages;
+       }
 
-    // see DbFile.java for javadocs
-    public List<Page> insertTuple(TransactionId tid, Tuple t)
-            throws DbException, IOException, TransactionAbortedException {
-        // some code goes here
-        return null;
-        // not necessary for lab1
-    }
+       // see DbFile.java for javadocs
+       public List<Page> insertTuple(TransactionId tid, Tuple t)
+                     throws DbException, IOException, TransactionAbortedException {
+              // some code goes here
+              return null;
+              // not necessary for lab1
+       }
 
-    // see DbFile.java for javadocs
-    public ArrayList<Page> deleteTuple(TransactionId tid, Tuple t) throws DbException,
-            TransactionAbortedException {
-        // some code goes here
-        return null;
-        // not necessary for lab1
-    }
+       // see DbFile.java for javadocs
+       public ArrayList<Page> deleteTuple(TransactionId tid, Tuple t) throws DbException,
+                     TransactionAbortedException {
+              // some code goes here
+              return null;
+              // not necessary for lab1
+       }
 
-    // see DbFile.java for javadocs
-    public DbFileIterator iterator(TransactionId tid) {
-        // some code goes here
-        return null;
-    }
+       // see DbFile.java for javadocs
+       public DbFileIterator iterator(TransactionId tid) {
+              return new DbFileIterator() {
+                     private int pageNo = 0;
+                     private Iterator<Tuple> tupleIterator = null;
+
+                     @Override
+                     public void open() {
+                            tupleIterator = getTupleIterator(0);
+                            advanceToNextNonEmptyPage();
+                     }
+
+                     @Override
+                     public boolean hasNext() {
+                            return tupleIterator.hasNext();
+                     }
+
+                     @Override
+                     public Tuple next() {
+                            if (!hasNext()) {
+                                   return null;
+                            }
+                            Tuple t = tupleIterator.next();
+                            if (!tupleIterator.hasNext()) {
+                                   pageNo++;
+                                   tupleIterator = getTupleIterator(pageNo);
+                                   advanceToNextNonEmptyPage();
+                            }
+                            return t;
+                     }
+
+                     @Override
+                     public void rewind() {
+                            open();
+                     }
+
+                     @Override
+                     public void close() {
+                            tupleIterator = null;
+                     }
+
+                     private Iterator<Tuple> getTupleIterator(int pageNo) {
+                            if (pageNo >= numPages()) {
+                                   return null;
+                            }
+                            HeapPageId pid = new HeapPageId(getId(), pageNo);
+                            try {
+                                   HeapPage page = (HeapPage) Database.getBufferPool().getPage(
+                                                 tid, pid, Permissions.READ_ONLY);
+                                   return page.iterator();
+                            } catch (TransactionAbortedException e) {
+                                   return null;
+                            } catch (DbException e) {
+                                   return null;
+                            }
+                     }
+
+                     private void advanceToNextNonEmptyPage() {
+                            while (tupleIterator != null && !tupleIterator.hasNext() && pageNo < numPages()) {
+                                   pageNo = pageNo + 1;
+                                   tupleIterator = getTupleIterator(pageNo);
+                            }
+                     }
+              };
+
+       }
 
 }
-
--- src/java/simpledb/storage/RecordId.java
+++ src/java/simpledb/storage/RecordId.java
@@ -8,60 +8,62 @@ import java.io.Serializable;
  */
 public class RecordId implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+       private static final long serialVersionUID = 1L;
+       private final PageId pid;
+       private final int tupleno;
 
-    /**
-     * Creates a new RecordId referring to the specified PageId and tuple
-     * number.
-     * 
-     * @param pid
-     *            the pageid of the page on which the tuple resides
-     * @param tupleno
-     *            the tuple number within the page.
-     */
-    public RecordId(PageId pid, int tupleno) {
-        // some code goes here
-    }
+       /**
+        * Creates a new RecordId referring to the specified PageId and tuple
+        * number.
+        * 
+        * @param pid
+        *                the pageid of the page on which the tuple resides
+        * @param tupleno
+        *                the tuple number within the page.
+        */
+       public RecordId(PageId pid, int tupleno) {
+              this.pid = pid;
+              this.tupleno = tupleno;
+       }
 
-    /**
-     * @return the tuple number this RecordId references.
-     */
-    public int getTupleNumber() {
-        // some code goes here
-        return 0;
-    }
+       /**
+        * @return the tuple number this RecordId references.
+        */
+       public int getTupleNumber() {
+              return tupleno;
+       }
 
-    /**
-     * @return the page id this RecordId references.
-     */
-    public PageId getPageId() {
-        // some code goes here
-        return null;
-    }
+       /**
+        * @return the page id this RecordId references.
+        */
+       public PageId getPageId() {
+              return pid;
+       }
 
-    /**
-     * Two RecordId objects are considered equal if they represent the same
-     * tuple.
-     * 
-     * @return True if this and o represent the same tuple
-     */
-    @Override
-    public boolean equals(Object o) {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-    }
+       /**
+        * Two RecordId objects are considered equal if they represent the same
+        * tuple.
+        * 
+        * @return True if this and o represent the same tuple
+        */
+       @Override
+       public boolean equals(Object o) {
+              if ((o instanceof RecordId)) {
+                     RecordId other = (RecordId) o;
+                     return tupleno == other.tupleno && pid.equals(other.pid);
+              }
+              return false;
+       }
 
-    /**
-     * You should implement the hashCode() so that two equal RecordId instances
-     * (with respect to equals()) have the same hashCode().
-     * 
-     * @return An int that is the same for equal RecordId objects.
-     */
-    @Override
-    public int hashCode() {
-        // some code goes here
-        throw new UnsupportedOperationException("implement this");
-
-    }
+       /**
+        * You should implement the hashCode() so that two equal RecordId instances
+        * (with respect to equals()) have the same hashCode().
+        * 
+        * @return An int that is the same for equal RecordId objects.
+        */
+       @Override
+       public int hashCode() {
+              return 31 * (pid.hashCode() + tupleno);
+       }
 
 }
